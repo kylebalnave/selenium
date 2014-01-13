e627039c4cc16c97443cf92f875ac73be12ff6ed
Kyle Balnave <balnavekyle@gmail.com>
2014-01-13 11:45:32 +0000

    Added full list of Selenium1 IDE commands

---
+++ interpreter/src/com/selenium/steps/selenium1/GetValue.java
@@ -0,0 +1,26 @@
+package com.selenium.steps.selenium1;
+
+import com.selenium.steps.*;
+import com.selenium.exceptions.StepException;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * @author kyleb2
+ */
+public class GetValue extends Step implements IStep {
+
+    public GetValue(Object... args) throws StepException {
+        super(args);
+    }
+
+    @Override
+    public WebDriver run(WebDriver wd) {
+        return wd;
+    }
+
+    @Override
+    public String getDescription() {
+        return String.format("@TODO add description");
+    }
+    
+}
